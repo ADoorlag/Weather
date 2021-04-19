ee37542373de2a99906df734db40d61eff2b8646
Alex Doorlag <Alex.Doorlag@gmail.com>
2021-04-19 18:49:55 -0400

    basic file structure created, main function to fetch api successfully created.

---
+++ main.js
@@ -0,0 +1,5 @@
+// const key = "7d8517efb0211f176985fee57d51facb"
+
+// fetch(`http://api.openweathermap.org/data/2.5/weather?q=detroit&appid=${key}`)
+// .then(response => {return response.json()})
+// .then(data => {console.log(data)})
