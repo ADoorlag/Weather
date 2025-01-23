6f12c2db838ea6ae471d50ed607c1f2f27f08e3a
ADoorlag <Alex.Doorlag@gmail.com>
2025-01-23 13:44:30 -0500

    made some final style changes and added JavaScript code to pull the user's current weather data from the API based on their current location

--- main.js
+++ main.js
@@ -1,27 +1,49 @@
+//variable declarations
+const cityInput = document.getElementById("city-input");
+const searchButton = document.getElementById("search-button");
+const city = document.getElementById("city");
+const temp = document.getElementById("temp");
+const icon = document.getElementById("icon");
+const description = document.getElementById("description");
+const lastUpdated = document.getElementById("last-updated");
+const feelsLike = document.getElementById("feels-like");
+const windSpeed = document.getElementById("wind-speed");
+const visibility = document.getElementById("visibility");
+const windGust = document.getElementById("wind-gust");
+const pressure = document.getElementById("pressure");
+const humidity = document.getElementById("humidity");
+const minTemp = document.getElementById("min-temp");
+const maxTemp = document.getElementById("max-temp");
+
 const key = "7d8517efb0211f176985fee57d51facb"
 
 // fetch(`http://api.openweathermap.org/data/2.5/weather?q=detroit&appid=${key}`)
 // .then(response => {return response.json()})
 // .then(data => {console.log(data)})
 
+// http://openweathermap.org/img/wn/03d@2x.png
+
+// get user's current coordinates and fetch weather data for that location
 navigator.geolocation.getCurrentPosition(position => {
     const lat = position.coords.latitude
     const lon = position.coords.longitude
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`)
     .then(response => {return response.json()})
     .then(data => {
-        const city = data.name
-        const temp = Math.round(data.main.temp - 273.15)
-        const icon = data.weather[0].icon
-        const desc = data.weather[0].description
-        const feelsLike = Math.round(data.main.feels_like - 273.15)
-        const windSpeed = data.wind.speed
-        const visibility = data.visibility
-        const windGust = data.wind.gust
-        const pressure = data.main.pressure
-        const humidity = data.main.humidity
-        
-        console.log(data)
-        console.log("city:", city,"temp:", temp, "icon:", icon, "desc:", desc, "feelsLike:", feelsLike, "windSpeed:", windSpeed, "visibility:", visibility, "Wind Gust:", windGust, "pressure:", pressure, "humidity:", humidity)
+
+        //display weather data
+        city.textContent = data.name
+        temp.innerHTML = Math.round(data.main.temp - 273.15) + "&#730<sup><span>F</span></sup>"
+        icon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+        description.textContent = data.weather[0].description
+        lastUpdated.textContent = `Updated as of ${new Date(data.dt * 1000).toLocaleString()}`
+        windSpeed.textContent = `${data.wind.speed} m/s`
+        visibility.textContent = `${data.visibility} meters`
+        windGust.textContent = `${data.wind.gust} m/s`
+        pressure.textContent = `${data.main.pressure} hPa`
+        humidity.textContent = `${data.main.humidity}%`
+        feelsLike.innerHTML = Math.round(data.main.feels_like - 273.15) + "&#730<sup><span>F</span></sup>"
+        minTemp.innerHTML = Math.round(data.main.temp_min - 273.15) + "&#730<sup><span>F</span></sup>"
+        maxTemp.innerHTML = Math.round(data.main.temp_max - 273.15) + "&#730<sup><span>F</span></sup>"
     })
 })
