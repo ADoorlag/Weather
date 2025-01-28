ecbe8a9242e1515563dd2fc4eb81663e5a657ba1
ADoorlag <Alex.Doorlag@gmail.com>
2025-01-28 10:04:56 -0500

    Final JavaScript changes for initial build for weather app

--- main.js
+++ main.js
@@ -17,39 +17,31 @@ const maxTemp = document.getElementById("max-temp");
 
 const key = "7d8517efb0211f176985fee57d51facb"
 
-// fetch(`http://api.openweathermap.org/data/2.5/weather?q=detroit&appid=${key}`)
-// .then(response => {return response.json()})
-// .then(data => {console.log(data)})
-
-// http://openweathermap.org/img/wn/03d@2x.png
-
 // get user's current coordinates and fetch weather data for that location
 navigator.geolocation.getCurrentPosition(position => {
     const lat = position.coords.latitude
     const lon = position.coords.longitude
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`)
     .then(response => {return response.json()})
-    .then(data => {
-
-        //display weather data
-        city.textContent = data.name
-        temp.innerHTML = Math.round(data.main.temp - 273.15) + "&#730<sup><span>F</span></sup>"
-        icon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-        description.textContent = data.weather[0].description
-        lastUpdated.textContent = `Updated as of ${new Date(data.dt * 1000).toLocaleString()}`
-        windSpeed.textContent = `${data.wind.speed} m/s`
-        visibility.textContent = `${(data.visibility/1000)} km`
-        windGust.textContent = `${data.wind.gust} m/s`
-        pressure.textContent = `${data.main.pressure} hPa`
-        humidity.textContent = `${data.main.humidity}%`
-        feelsLike.innerHTML = Math.round(data.main.feels_like - 273.15) + "&#730<sup><span>F</span></sup>"
-        minTemp.innerHTML = Math.round(data.main.temp_min - 273.15) + "&#730<sup><span>F</span></sup>"
-        maxTemp.innerHTML = Math.round(data.main.temp_max - 273.15) + "&#730<sup><span>F</span></sup>"
+    .then (data => {
+    city.textContent = data.name
+    temp.innerHTML = Math.round(data.main.temp - 273.15) + "&#730<sup><span>F</span></sup>"
+    icon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+    description.textContent = data.weather[0].description
+    lastUpdated.textContent = `Updated as of ${new Date(data.dt * 1000).toLocaleString()}`
+    windSpeed.textContent = `${data.wind.speed} m/s`
+    visibility.textContent = `${(data.visibility/1000)} km`
+    windGust.textContent = `${data.wind.gust} m/s`
+    pressure.textContent = `${data.main.pressure} hPa`
+    humidity.textContent = `${data.main.humidity}%`
+    feelsLike.innerHTML = Math.round(data.main.feels_like - 273.15) + "&#730<sup><span>F</span></sup>"
+    minTemp.innerHTML = Math.round(data.main.temp_min - 273.15) + "&#730<sup><span>F</span></sup>"
+    maxTemp.innerHTML = Math.round(data.main.temp_max - 273.15) + "&#730<sup><span>F</span></sup>"
     })
 })
 
 // search for weather data for a specific city
-searchButton.addEventListener("click", () => {
+function searchWeather() {
     const cityName = cityInput.value;
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}`)
     .then(response => {return response.json()})
@@ -71,4 +63,14 @@ searchButton.addEventListener("click", () => {
     })
 
     cityInput.value = ""
+}
+
+searchButton.addEventListener("click", () => {
+    searchWeather();
+})
+
+document.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        searchWeather();
+    }
 })
